f14178d9d5be796a103b890f2be6143bafdc14e1
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-03-08 15:34:06 +0100

    Add e2e tests and CI testing

---
+++ cypress/integration/ui_spec.js
@@ -0,0 +1,24 @@
+const {
+  adminUrl,
+  user: { email, password }
+} = Cypress.env()
+
+describe('Strapi Login flow', () => {
+  it('visit the Strapi admin panel', () => {
+    cy.visit(adminUrl)
+    cy.get('form', { timeout: 10000 }).should('be.visible')
+  })
+
+  it('Fill the login form', () => {
+    cy.get('input[name="email"]').type(email).should('have.value', email)
+    cy.get('input[name="password"]').type(password).should('have.value', password)
+    cy.get('button[type="submit"]').click()
+  })
+
+  it('Enter to the plugin Home Page', () => {
+    cy.contains('meilisearch', { timeout: 10000 }).click()
+    cy.url().should('include', '/plugins/meilisearch')
+  })
+
+  // @todo add [ CREATE | EDIT | DELETE ] tests
+})
